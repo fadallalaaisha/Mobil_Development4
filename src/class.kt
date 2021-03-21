858adf9c038f1fadfd9cf78e8f6d9fa2cea38f12
fadallalaaisha <aishafadallala@gmail.com>
2021-03-21 11:20:17 +0300

    Assignment4

--- src/class.kt
+++ src/class.kt
@@ -19,7 +19,7 @@ fun numberArray(){
     var number= arrayOf(32,17,4,213,78,43,90,31,3,73,11,158,62)
     println(number[2])
     println(number[5])
-    var index=number.indexOf(158) //still
+    var index=number.indexOf(158) //showing the index position
     println(index)
     for (oneNumber in number.sorted()){
         println(oneNumber)
@@ -27,7 +27,7 @@ fun numberArray(){
 }
 fun names(num1:String,num2:String,num3:String){
     var name= arrayOf("$num1 ","$num2", "$num3" )
-    println(Arrays.toString(name)) //not suer
+    println(Arrays.toString(name)) // function to take in 3 Strings anr returns them all.
 }
 
 
